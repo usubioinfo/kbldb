deefe65cd2eff5b062a2630af0adb9cbf9ebfa8c
MusicDev33 <smccowan33@gmail.com>
2020-11-12 22:07:40 -0700

    Logger

--- src/uptime.ts
+++ src/uptime.ts
@@ -2,3 +2,4 @@
 // tools on our server. Having tools randomly not being available is
 // really annoying to keep track of, and I'd like to have a way of
 // keeping track of uptime.
+import Logger from '@config/logger';
