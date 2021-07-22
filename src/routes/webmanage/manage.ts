695f1e376989c5c6b0e9fef17dea35f8171ebfc6
MusicDev33 <smccowan33@gmail.com>
2021-07-22 15:33:19 -0600

    can now update sites from dash

--- src/routes/webmanage/manage.ts
+++ src/routes/webmanage/manage.ts
@@ -5,10 +5,16 @@ export const updateSiteRoute = async (req: Request, res: Response) => {
   const body = req.body;
   const siteName = req.body.site;
 
+  const sites = ['bioinformatics', 'bioinfocore', 'biocluster'];
+
+  if (!sites.includes(siteName)) {
+    return res.json({success: false, msg: `Incorrect site name ${siteName}`});
+  }
+
   console.log(req.get('origin'));
   console.log(siteName);
 
-  const { stdout, stderr } = await exec('who');
+  const { stdout, stderr } = await exec(`bxz update ${siteName}`);
 
   return res.json({success: true, msg: stdout});
 
