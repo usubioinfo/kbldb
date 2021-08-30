dfdea1d5b449d7a0603f043a3adf89438f09ca14
MusicDev33 <smccowan33@gmail.com>
2021-08-30 13:48:01 -0600

    Add support for caching modules

--- src/routes/modfiles/getters.ts
+++ src/routes/modfiles/getters.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 const {exec} = require('child-process-async');
+import cron from 'node-cron';
 
 import { LinkMap } from './config';
 
@@ -12,11 +13,17 @@ const MOD_FILES = process.env.MOD_FILES;
 
 const EXCLUDE = ['test', 'template', 'bin'];
 
+let modules: {module: string, versions: string[], name: string, link: string}[] = [];
+
+cron.schedule('0 4 * * *', () => {
+  modules = [];
+});
+
 export const getAvailModulesRoute = async (req: Request, res: Response) => {
   const lsResults: ExecOutput = await exec(`ls ${MOD_FILES}`);
 
   // tbh this is kinda insane, this is definitely something to clean up in the future
-  let modules = await Promise.all(lsResults.stdout.replace(/\s+/g, ' ').trim().split(' ').filter(module => {
+  modules = await Promise.all(lsResults.stdout.replace(/\s+/g, ' ').trim().split(' ').filter(module => {
     if(EXCLUDE.includes(module)) {
       return false;
     }
@@ -42,7 +49,9 @@ export const getAvailModulesRoute = async (req: Request, res: Response) => {
       return `${module}/${version}`;
     });
 
-    return {module, versions, name: metadata.name, link: metadata.link};
+    const data = {module, versions, name: metadata.name, link: metadata.link};
+
+    return data;
   }));
 
 
