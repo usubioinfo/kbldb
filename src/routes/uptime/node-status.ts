cfd8f7a0ef4ecbd088f83f42cbae510cd2c057c8
MusicDev33 <smccowan33@gmail.com>
2021-09-07 13:43:39 -0600

    Status route for nodes

---
+++ src/routes/uptime/node-status.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import axios from 'axios';
+
+type Node = {
+  name: string,
+  ip: string
+}
+
+const nodes: Node[] = [
+  {name: 'chela01', ip: '10.1.1.1'},
+  {name: 'chela02', ip: '10.1.1.2'},
+  {name: 'chela03', ip: '10.1.1.3'},
+  {name: 'chela04', ip: '10.1.1.4'},
+  {name: 'chela05', ip: '10.1.1.5'},
+  {name: 'chela-g01', ip: '10.1.1.11'},
+];
+
+const getNodeByName = (nodeName: string) => {
+  const foundNode = nodes.filter(node => {
+    return node.name === nodeName;
+  });
+
+  if (foundNode.length) {
+    return foundNode[0];
+  }
+
+  return false;
+}
+
+export const getNodeStatusRoute = async (req: Request, res: Response) => {
+  type Result = {
+    status: 'online' | 'offline',
+    node: string
+  }
+
+  let results: Result[] = [];
+
+  for (let node of nodes) {
+    try {
+      const res = await axios.get(node.ip);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return res.json({success: true});
+}
--- src/routes/uptime/routes.ts
+++ src/routes/uptime/routes.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import * as RouteFunctions from './export';
 
 router.get('/uptimes', RouteFunctions.getAllUptimesRoute);
+router.get('/nodes', RouteFunctions.getNodeStatusRoute);
 
 const UptimeRoutes = router;
 export default UptimeRoutes;
