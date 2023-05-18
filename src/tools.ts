f6e73f826f1ede6e862413ac708d5a83f01fa8d4
navduhan <duhan27dec@gmail.com>
2023-05-17 22:41:56 -0500

    https update

--- src/tools.ts
+++ src/tools.ts
@@ -1,47 +1,47 @@
 export const tools: {name: string, link: string, availability: string}[] = [
   {
     name: 'legumeSSRdb',
-    link: 'http://bioinfo.usu.edu/legumeSSRdb/',
+    link: 'https://bioinfo.usu.edu/legumeSSRdb/',
     availability: ''
   },
   {
     name: 'citSATdb',
-    link: 'http://bioinfo.usu.edu/citSATdb',
+    link: 'https://bioinfo.usu.edu/citSATdb',
     availability: ''
   },
   {
     name: 'PredHPI',
-    link: 'http://bioinfo.usu.edu/PredHPI/',
+    link: 'https://bioinfo.usu.edu/PredHPI/',
     availability: ''
   },
   {
     name: 'alfaNET',
-    link: 'http://bioinfo.usu.edu/alfanet/',
+    link: 'https://bioinfo.usu.edu/alfanet/',
     availability: ''
   },
   {
     name: 'WeCoNET',
-    link: 'http://bioinfo.usu.edu/weconet/',
+    link: 'https://bioinfo.usu.edu/weconet/',
     availability: ''
   },
   {
     name: 'AP-iNET',
-    link: 'http://bioinfo.usu.edu/AP-iNET',
+    link: 'https://bioinfo.usu.edu/AP-iNET',
     availability: ''
   },
   {
     name: 'deepHPI',
-    link: 'http://bioinfo.usu.edu/deepHPI/',
+    link: 'https://bioinfo.usu.edu/deepHPI/',
     availability: ''
   },
   {
     name: 'HuCoPIA',
-    link: 'http://bioinfo.usu.edu/hucopia/',
+    link: 'https://bioinfo.usu.edu/hucopia/',
     availability: ''
   },
   {
     name: 'Plant-mSubP',
-    link: 'http://bioinfo.usu.edu/Plant-mSubP',
+    link: 'https://bioinfo.usu.edu/Plant-mSubP',
     availability: ''
   },
   {
@@ -56,32 +56,32 @@ export const tools: {name: string, link: string, availability: string}[] = [
   },
   {
     name: 'PLpred',
-    link: 'http://bioinfo.usu.edu/PLpred',
+    link: 'https://bioinfo.usu.edu/PLpred',
     availability: ''
   },
   {
     name: 'deepNEC',
-    link: 'http://bioinfo.usu.edu/deepNEC/',
+    link: 'https://bioinfo.usu.edu/deepNEC/',
     availability: ''
   },
   {
     name: 'GreeningDB',
-    link: 'http://bioinfo.usu.edu/GreeningDB/',
+    link: 'https://bioinfo.usu.edu/GreeningDB/',
     availability: ''
   },
   {
     name: 'ranchSATdb',
-    link: 'http://bioinfo.usu.edu/ranchSATdb/',
+    link: 'https://bioinfo.usu.edu/ranchSATdb/',
     availability: ''
   },
   {
     name: 'LacSubPred',
-    link: 'http://bioinfo.usu.edu/lacsubpred',
+    link: 'https://bioinfo.usu.edu/lacsubpred',
     availability: ''
   },
   {
     name: 'LigPred',
-    link: 'http://bioinfo.usu.edu/ligpred',
+    link: 'https://bioinfo.usu.edu/ligpred',
     availability: ''
   },
   {
@@ -91,32 +91,32 @@ export const tools: {name: string, link: string, availability: string}[] = [
   },
   {
     name: 'Malivhu',
-    link: 'http://bioinfo.usu.edu/malivhu/',
+    link: 'https://bioinfo.usu.edu/malivhu/',
     availability: ''
   },
   {
     name: 'TRustDB',
-    link: 'http://bioinfo.usu.edu/trustdb/',
+    link: 'https://bioinfo.usu.edu/trustdb/',
     availability: ''
   },
   {
     name: 'HuCoVarIA',
-    link: 'http://bioinfo.usu.edu/hucovaria/',
+    link: 'https://bioinfo.usu.edu/hucovaria/',
     availability: ''
   },
   {
     name: 'ProFeatX',
-    link: 'http://bioinfo.usu.edu/profeatx/',
+    link: 'https://bioinfo.usu.edu/profeatx/',
     availability: ''
   },
   {
     name: 'pySeqRNA',
-    link: 'http://bioinfo.usu.edu/pyseqrna/',
+    link: 'https://bioinfo.usu.edu/pyseqrna/',
     availability: ''
   },
   {
     name: 'TritiKBdb',
-    link: 'http://bioinfo.usu.edu/tritikbdb',
+    link: 'https://bioinfo.usu.edu/tritikbdb',
     availability: ''
   }
 ];
