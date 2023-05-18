925c1017da978667316307f89a8c17f70615260d
Shelby McCowan <smccowan33@gmail.com>
2023-05-17 21:04:01 -0700

    Update uptime.ts

--- src/uptime.ts
+++ src/uptime.ts
@@ -6,13 +6,21 @@ import Logger from '@config/logger';
 import axios from 'axios';
 import { addNewUptime } from '@routes/uptime/creation';
 
+import * as https from 'https';
+
+// TODO: Fix this! This is a security hazard!
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
+// Use this to actually set up SSL
+// const httpsAgent = new https.Agent({ ca: MY_CA_BUNDLE_PATH });
+
 export const toolCheck = async (tools: {name: string, link: string, availability: string}[]) => {
   for (const tool of tools) {
     // Set to slow, and if it takes a while to load, well...it's slow then
     tool.availability = 'slow';
 
     try {
-      const response = await axios.get(tool.link);
+      const response = await axios.get(tool.link, { httpsAgent });
       console.log(`${tool.name} - ${response.status}`);
       
       tool.availability = 'running';
