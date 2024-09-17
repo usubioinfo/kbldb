8b6da10008d7160307b76d5674b3a68018fc0cb9
navduhan <duhan27dec@gmail.com>
2024-09-17 14:34:05 -0500

    updated dockerization of kbldb

--- init-mongo.js
+++ init-mongo.js
@@ -1,3 +1,6 @@
 db = connect("mongodb://localhost:27017/kbl");
 // Create collection "uptimes" if it doesn't exist
-db.createCollection("uptimes"):
+db.createCollection("uptimes");
+db.createCollection("appointments");
+db.createCollection("people");
+db.createCollection("newsarticles");
